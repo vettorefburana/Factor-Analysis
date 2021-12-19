c4a321a41ddd15f25e24c6f505e899dcca28e5dd
vettorefburana <74903161+vettorefburana@users.noreply.github.com>
2021-12-19 21:15:45 +0100

    Add files via upload

--- requirements.r
+++ requirements.r
@@ -1,11 +1,16 @@
 
+
 install.packages("markdown")
 install.packages("MASS")
 install.packages("xts")
 install.packages("ggcorrplot")
 install.packages("kableExtra")
-install.packages("keras")
-install.packages("tensorflow")
 install.packages("NeuralNetTools")
 install.packages("Hmisc")
 install.packages("olsrr")
+
+# install keras and tensorflow
+install.packages("remotes")
+remotes::install_github(paste0("rstudio/", c("reticulate", "tensorflow", "keras")))
+reticulate::install_miniconda() 
+keras::install_keras()
